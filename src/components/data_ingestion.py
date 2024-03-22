6c1c63b0c1e9c5e37a2fe71f37852d46bf62a0d2
Valiant2110 <vipul.v.gotiwale@gmail.com>
2024-03-22 23:44:14 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -36,6 +36,6 @@ class DataIngestion:
             raise customException(e,sys)
             
             
-if __name__ == '__main__':
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+# if __name__ == '__main__':
+#     obj = DataIngestion()
+#     obj.initiate_data_ingestion()
